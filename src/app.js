0da76c784f4672cbcb01b868c2fa2ea12e26b187
kausar2697 <17103126@iubat.edu>
2020-06-11 15:57:53 +0600

    final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//*********************************IF ELSE */
+//*********************************IF ELSE *******************************/
 
 function changeToInt(v){
     let result = Number.parseInt(v)
@@ -10,3 +10,81 @@ function changeToInt(v){
 
 let result =changeToInt('877.32')
 console.log(result)
+
+//##########################################################//#endregion
+
+//**********************************try catch ***************************/
+
+function makeWords(text){
+ try{
+     let str = text.trim()
+     let words =str.split(' ')
+     return words
+ }catch(e){
+      console.log(e.message)  //error message
+ }
+}
+
+// let words = makeWords('    32    kausar islam           ')    //(6) ["32", "", "", "", "kausar", "islam"]
+let words = makeWords(67)
+console.log(words)       //text.trim is not a function
+
+//########################################################################//#endregion
+
+//****************************throw an error***************************************** */
+
+try{
+    console.log('line 1')
+    console.log('line 2')
+    throw new Error('this is the error')  //custom error
+    
+    console.log('line 3')
+    console.log('line 4')
+}catch(e){
+    console.log(e.message)
+}
+
+//########################################################################//#endregion
+
+//***************************Finally block ****************************** */
+
+try{
+    console.log('line 1')
+    console.log('line 2')
+    throw new Error('this is the error')  //custom error
+    
+    console.log('line 3')
+    console.log('line 4')
+}catch(e){
+    console.log(e.message)
+}finally{
+    console.log('finally block')  //always  be execute 
+}
+
+
+//#####################################################################//#endregion
+
+//**************************************Custom error */
+
+
+class CustomError extends Error{
+    constructor(msg) {
+        super(msg)
+        if(Error.captureStackTrace){
+            Error.captureStackTrace(this,CustomError)
+        }
+        
+    }
+}
+try{
+        console.log('line 1')
+        console.log('line 2')
+        throw new CustomError('this is the error')  //custom error
+        
+        console.log('line 3')
+        console.log('line 4')
+    }catch(e){
+        console.dir(e)
+    }finally{
+        console.log('finally block')  //always  be execute 
+    }
